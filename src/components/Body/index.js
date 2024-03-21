1e2690650974fbd4e10f3072b8b083e83fdad54e
Naveen <pothurajunaveen963@gmail.com>
2024-03-21 00:58:44 +0000

    Success

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -6,37 +6,43 @@ const Body = () => (
   <ConfigurationContext.Consumer>
     {value => {
       const {showContent, showLeftNavbar, showRightNavbar} = value
-      const leftNavbarStatus = showLeftNavbar ? 'show-left-navbar' : 'hide'
+      const leftNavbarStatus = showLeftNavbar ? 'show-left-navbar' : null
       const contentStatus = showContent ? 'show-content' : 'hide'
-      const rightNavbarStatus = showRightNavbar ? 'show-right-navbar' : 'hide'
+      const rightNavbarStatus = showRightNavbar ? 'show-right-navbar' : null
       console.log(showContent, showLeftNavbar, showRightNavbar)
 
       return (
         <div className="body-container">
-          <div className={leftNavbarStatus}>
-            <h1 className="heading">Left Navbar Menu</h1>
-            <ul className="left-navbar-ul">
-              <li>Item 1</li>
-              <li>Item 2</li>
-              <li>Item 3</li>
-              <li>Item 4</li>
-            </ul>
-          </div>
-          <div className={contentStatus}>
-            <h1 className="heading">Content</h1>
-            <p className="content-para">Lorem ipsum dolor</p>
-          </div>
-          <div className={rightNavbarStatus}>
-            <h1 className="heading">Right Navbar</h1>
-            <div className="ad-boxes-container">
-              <div className="ad-box">
-                <p className="ad-text">Ad 1</p>
-              </div>
-              <div className="ad-box">
-                <p className="ad-text">Ad 2</p>
+          {showLeftNavbar ? (
+            <div className={leftNavbarStatus}>
+              <h1 className="heading">Left Navbar Menu</h1>
+              <ul className="left-navbar-ul">
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+              </ul>
+            </div>
+          ) : null}
+          {showContent ? (
+            <div className={contentStatus}>
+              <h1 className="heading">Content</h1>
+              <p className="content-para">Lorem ipsum dolor</p>
+            </div>
+          ) : null}
+          {showRightNavbar ? (
+            <div className={rightNavbarStatus}>
+              <h1 className="heading">Right Navbar</h1>
+              <div className="ad-boxes-container">
+                <div className="ad-box">
+                  <p className="ad-text">Ad 1</p>
+                </div>
+                <div className="ad-box">
+                  <p className="ad-text">Ad 2</p>
+                </div>
               </div>
             </div>
-          </div>
+          ) : null}
         </div>
       )
     }}
